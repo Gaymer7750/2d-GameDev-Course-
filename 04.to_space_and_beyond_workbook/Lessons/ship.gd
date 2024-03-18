de46535e23c2c6a4ab159f28f41612179959c9f5
Gaymer7750 <zhcheng2@s.sfusd.edu>
2024-03-18 11:44:43 -0700

    L4 and Practice 4

--- 04.to_space_and_beyond_workbook/Lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/Lessons/ship.gd
@@ -1,18 +1,28 @@
 extends Sprite2D
+var boost_speed := 1500.0
+var normal_speed := 600.0
 
-var max_speed := 600.0
+var max_speed := normal_speed
 var velocity := Vector2(0, 0)
 
+var direction := Vector2(0, 0)
 func _process(delta: float) -> void:
-	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 
-	velocity = direction * max_speed
+	if Input.is_action_just_pressed("boost"):
+		normal_speed = boost_speed
+		get_node("Timer").start()
+
+	velocity = direction * normal_speed
 	position += velocity * delta
 
 	if direction. length() > 0.0:
 		rotation = velocity.angle()
+
+
+func _on_timer_timeout():
+	normal_speed = max_speed
