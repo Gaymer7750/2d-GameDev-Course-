1c568883600a631c9498b93cdbe5f881d61962de
Gaymer7750 <zhcheng2@s.sfusd.edu>
2024-04-26 12:34:03 -0700

    L5 P1 and 2

--- M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -3,8 +3,10 @@ extends Area2D
 @onready var animation_player: AnimationPlayer = $"AnimationPlayer"
 
 
+
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
-	# Complete this boolean expression to check if the input event is a left-mouse click.
+	animation_player.play("open")
+	input_pickable = false
 	var event_is_mouse_click: bool = (
 		false
 	)
